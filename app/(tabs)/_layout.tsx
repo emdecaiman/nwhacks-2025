99e6fb2ed17132b5ea33fca3be48255e648d0b4a
Emmanuel de Caiman <emdecaiman@gmail.com>
2025-01-18 13:18:06 -0800

    styled tabs and status bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,56 @@
+import React from 'react';
 import { Tabs } from 'expo-router';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { StatusBar } from 'expo-status-bar';
 
 export default function TabLayout() {
-  return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: 'Home' }} />
-      <Tabs.Screen name="about" options={{ title: 'About' }} />
-      <Tabs.Screen name="settings" options={{ title: 'Settings' }} />
-    </Tabs>
-  );
+    return (
+        <>
+            <StatusBar style='auto' />
+            <Tabs
+                screenOptions={{
+                    headerStyle: {
+                        backgroundColor: '#25292e',
+                    },
+                    headerShadowVisible: false,
+                    headerTintColor: '#fff',
+                    tabBarStyle: {
+                        backgroundColor: '#25292e',
+                        height: 55,
+                    },
+                }}
+            >
+                <Tabs.Screen
+                    name="index"
+                    options={{
+                        title: 'Home',
+                        headerShown: false,
+                        tabBarIcon: ({ color, focused }) => (
+                            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+                        ),
+                    }}
+                />
+                <Tabs.Screen
+                    name="history"
+                    options={{
+                        title: 'History',
+                        headerShown: false,
+                        tabBarIcon: ({ color, focused }) => (
+                            <Ionicons name={focused ? 'information-circle-sharp' : 'information-circle-outline'} color={color} size={24} />
+                        ),
+                    }}
+                />
+                <Tabs.Screen
+                    name="settings"
+                    options={{
+                        title: 'Settings',
+                        headerShown: false,
+                        tabBarIcon: ({ color, focused }) => (
+                            <Ionicons name={focused ? 'settings-sharp' : 'settings-outline'} color={color} size={24} />
+                        ),
+                    }}
+                />
+            </Tabs>
+        </>
+    );
 }
