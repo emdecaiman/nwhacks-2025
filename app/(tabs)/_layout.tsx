fcc3f0ffb40550f53c94e6cd4551515a1dd880ac
Emmanuel de Caiman <emdecaiman@gmail.com>
2025-01-18 12:58:08 -0800

    created working tabs

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,12 @@
+import { Tabs } from 'expo-router';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+export default function TabLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen name="index" options={{ title: 'Home' }} />
+      <Tabs.Screen name="about" options={{ title: 'About' }} />
+      <Tabs.Screen name="settings" options={{ title: 'Settings' }} />
+    </Tabs>
+  );
+}
